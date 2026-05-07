9d32eed1f01a8a12d09f3a857dbf1670c81da499
Al Pathyno <fathyharisyazkar48@gmail.com>
2026-05-07 10:09:49 +0700

    messi

--- api/index.php
+++ api/index.php
@@ -1,2 +1,12 @@
 <?php
+
+// Memaksa Laravel menggunakan folder /tmp untuk semua cache dan file sementara
+putenv('VIEW_COMPILED_PATH=/tmp');
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_EVENTS_CACHE=/tmp/events.php');
+putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+putenv('APP_SERVICES_CACHE=/tmp/services.php');
+
+// Memanggil file utama Laravel
 require __DIR__ . '/../public/index.php';
